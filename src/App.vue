fd1c09eaee4f21b823ab0dc005dd85dfcd2eca4c
GabrielGomezDlc <u202020341@upc.edu.pe>
2022-09-07 02:25:34 -0500

    SegundaClase

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,133 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
+export default {
+  data() {
+    return {
+      price:0,
+      date:null,
+      status:null,
+      selectedCategory: null,
+      subjectToTax:null,
+      categories: [
+        {name: 'New York', code: 'NY'},
+        {name: 'Rome', code: 'RM'},
+        {name: 'London', code: 'LDN'},
+        {name: 'Istanbul', code: 'IST'},
+        {name: 'Paris', code: 'PRS'}
+      ]
+    }
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <form>
 
-    <div class="wrapper">
-      <HelloWorld msg="Se que oyes mi pensamientos muchacho! " />
+    <div class="card">
+      <div class="field grid">
+          <label for="code" class="col-12 mb-2 md:col-2 md:mb-0">Código*</label>
+        <div class="col-12 md:col-10">
+          <InputText id="code"/>
+          <Button label="Validar"></Button>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="name" class="col-12 mb-2 md:col-2 md:mb-0">Nombre*</label>
+        <div class="col-12 md:col-10">
+          <InputText id="name"/>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="abbreviation" class="col-12 mb-2 md:col-2 md:mb-0">Abreviatura*</label>
+        <div class="col-12 md:col-10">
+          <InputText id="abbreviation"/>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="category" class="col-12 mb-2 md:col-2 md:mb-0">Categoría*</label>
+        <div class="col-12 md:col-10">
+          <Dropdown id="category" v-model="selectedCategory" :options="categories" optionLabel="name" placeholder="Selecciona una categoría"/>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="ruc" class="col-12 mb-2 md:col-2 md:mb-0">RUC Proveedor*</label>
+        <div class="col-12 md:col-10">
+          <InputMask id="ruc" v-model="value" mask="9999999999-999"/>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="price" class="col-12 mb-2 md:col-2 md:mb-0">Precio*</label>
+        <div class="col-12 md:col-10">
+          <InputNumber  id="price" v-model="price" showButtons mode="decimal" :min="0" />
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="label-color" class="col-12 mb-2 md:col-2 md:mb-0">Color de etiqueta*</label>
+        <div class="col-12 md:col-10">
+          <ColorPicker  id="label-color" v-model="color"/>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="date" class="col-12 mb-2 md:col-2 md:mb-0">Fecha*</label>
+        <div class="col-12 md:col-10">
+          <Calendar id="date" v-model="date" showIcon dateFormat="mm/dd/yy"/>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="status" class="col-12 mb-2 md:col-2 md:mb-0">Estado*</label>
+        <div class="col-12 md:col-10">
+          <RadioButton  id="status" value="Active" v-model="status" />
+          <label  class="col-12 mb-2 md:col-2 md:mb-0">Activo</label>
+          <RadioButton  id="status" value="Suspended" v-model="status" />
+          <label  class="col-12 mb-2 md:col-2 md:mb-0">Suspendido</label>
+          <RadioButton  id="status" value="Disabled" v-model="status" />
+          <label  class="col-12 mb-2 md:col-2 md:mb-0">De baja</label>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="subject-to-tax" class="col-12 mb-2 md:col-2 md:mb-0">Afecto a impuesto*</label>
+        <div class="col-12 md:col-10">
+          <TriStateCheckbox v-model="subjectToTax" />
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="photo" class="col-12 mb-2 md:col-2 md:mb-0">Foto</label>
+        <div class="col-12 md:col-10">
+          <FileUpload for="photo" name="demo[]" url="./upload" accept="image/*" />
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="description" class="col-12 mb-2 md:col-2 md:mb-0">Description</label>
+        <div class="col-12 md:col-10">
+          <Textarea for="description" v-model="description" :autoResize="true" rows="5" cols="30" />
+        </div>
+      </div>
+
+      <div class="field grid">
+
+        <div class="col-12 md:col-10">
+          <Button label="Grabar" class="p-button-outlined" icon="pi pi-save" iconPos="right"/>
+          <Button label="Limpiar" class="p-button-outlined" icon="pi pi-trash" iconPos="right"/>
+        </div>
+      </div>
+
+
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  </form>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import PrimeVue from 'primevue/config';
 import './assets/main.css'
+import Button from "primevue/button";
+import InputText from 'primevue/inputtext';
+import InputMask from 'primevue/inputmask';
+import InputNumber from 'primevue/inputnumber';
+import Dropdown from 'primevue/dropdown';
+import ColorPicker from 'primevue/colorpicker';
+import Calendar from 'primevue/calendar';
+import InputSwitch from 'primevue/inputswitch';
+import RadioButton from 'primevue/radiobutton';
+import TriStateCheckbox from 'primevue/tristatecheckbox';
+import FileUpload from 'primevue/fileupload';
+import Textarea from 'primevue/textarea';
 
-createApp(App).mount('#app')
+
+import 'primevue/resources/themes/md-dark-indigo/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+import '/node_modules/primeflex/primeflex.css'
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.component('InputMask', InputMask);
+app.component('InputNumber', InputNumber);
+app.component('Dropdown', Dropdown);
+app.component('ColorPicker', ColorPicker);
+app.component('Calendar', Calendar);
+app.component('InputSwitch', InputSwitch);
+app.component('RadioButton', RadioButton);
+app.component('TriStateCheckbox', TriStateCheckbox);
+app.component('FileUpload', FileUpload);
+app.component('Textarea', Textarea);
+
+app.mount('#app')
